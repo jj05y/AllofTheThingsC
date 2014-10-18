e62b4da825c0e4e640ef0dc51a3909714e884f34
joeyD <joey.duffin@gmail.com>
2014-10-18 08:40:13 +0100

    selection sort added

--- SortAlgorithms/src/SortAlgorithms.c
+++ SortAlgorithms/src/SortAlgorithms.c
@@ -11,7 +11,7 @@
 
 #define ARRAY_LEN 20
 
-void populate(int myInts[]) {
+void randomize(int myInts[]) {
 	int i;
 	srand(time(NULL));
 	for (i = 0; i < ARRAY_LEN; i++) {
@@ -27,11 +27,18 @@ void printInts(int myInts[]) {
 	puts("\n");
 }
 
-void swap(int myInts[], int i) {
+void swapIPlusOne(int myInts[], int i) {
 	int temp = myInts[i];
 	myInts[i] = myInts[i+1];
 	myInts[i+1] = temp;
 }
+
+void swapIJ(int myInts[], int i, int j) {
+	int temp = myInts[i];
+	myInts[i] = myInts[j];
+	myInts[j] = temp;
+}
+
 
 void bubbleSort(int myInts[]) {
 	int swapped = 1; //boolean
@@ -40,9 +47,25 @@ void bubbleSort(int myInts[]) {
 		swapped = 0;
 		for (i = 0; i < ARRAY_LEN -1; i++) {
 			if (myInts[i] > myInts[i+1]) {
-				swap(myInts, i);
+				swapIPlusOne(myInts, i);
 				swapped = 1;
 			}
+		}
+	}
+}
+
+void selectionSort(int myInts[]) {
+	int minI;
+	int i, j;
+	for (i = 0; i < ARRAY_LEN -1; i++) {
+		minI = i;
+		for (j = i + 1; j < ARRAY_LEN; j++) {
+			if (myInts[j] < myInts[minI]) {
+				minI = j;
+			}
+		}
+		if (minI != i) {
+			swapIJ(myInts, i, minI);
 		}
 	}
 }
@@ -50,15 +73,25 @@ void bubbleSort(int myInts[]) {
 int main(void) {
 	int myInts[ARRAY_LEN];
 
-	populate(myInts);
+	randomize(myInts);
 
 	puts("Unsorted array:");
 	printInts(myInts);
 
 	bubbleSort(myInts);
-
-	puts("Sorted array:");
+	puts("Bubble Sorted array:");
 	printInts(myInts);
+
+	randomize(myInts);
+
+	puts("\nUnorted Array");
+	printInts(myInts);
+
+	selectionSort(myInts);
+	puts("Selection Sorted Array");
+	printInts(myInts);
+
+	return 0;
 
 }
 
